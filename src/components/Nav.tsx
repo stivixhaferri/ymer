b2763d1f55ba86982d6f8735f8c07e201710ab55
StiviXhaferri <stivixhaferri01@gmail.com>
2025-02-10 21:43:24 +0100

    link in nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
-'use client'
+"use client";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import {  Phone } from 'lucide-react';
+import { Phone } from "lucide-react";
 import MobileMenu from "./MobileMenu";
 
 const Nav = () => {
@@ -9,27 +9,32 @@ const Nav = () => {
 
   useEffect(() => {
     const path = window.location.pathname;
-    if (path.includes('dashboard') || path.includes('login') || path.includes('register')) {
+    if (
+      path.includes("dashboard") ||
+      path.includes("login") ||
+      path.includes("register")
+    ) {
       setIsSpecialPage(true);
     } else {
       setIsSpecialPage(false);
     }
-  }, []); 
+  }, []);
 
   if (isSpecialPage) {
-    return <div></div>; 
+    return <div></div>;
   }
 
   return (
     <div className="flex py-2 px-[5%] items-center">
       {/* Logo Section */}
       <div className="w-[50%]">
-        <Link href={'/'}>
-        <img
-          src="/fulllogo_transparent_nobuffer.png"
-          className="w-[70px] object-fit"
-          alt="Logo"
-        /></Link>
+        <Link href={"/"}>
+          <img
+            src="/fulllogo_transparent_nobuffer.png"
+            className="w-[70px] object-fit"
+            alt="Logo"
+          />
+        </Link>
       </div>
 
       {/* Navigation Links (Desktop version) */}
@@ -43,8 +48,15 @@ const Nav = () => {
         </a>
 
         <a
-          href="https://www.google.com/maps/place/Ymer's+Grill/@41.323734,19.4809879,17z/data=!3m1!4b1!4m6!3m5!1s0x134fd9e63e845d2d:0xfff9728ecd454c!8m2!3d41.323734!4d19.4835628!16s%2Fg%2F11sm6nvt06?entry=ttu&g_ep=EgoyMDI1MDExNS4wIKXMDSoASAFQAw%3D%3D"
+          href="https://www.google.com/maps/place/Ymer%E2%80%99s+Grill/@41.2919408,19.5078317,19.49z/data=!4m15!1m8!3m7!1s0x134fd9089610cb59:0x9133b62c41269fef!2zUGxlcGEsIER1cnLDq3M!3b1!8m2!3d41.2920646!4d19.5081598!16s%2Fg%2F11kj82x4mc!3m5!1s0x134fd9131735ca1f:0x406fb486c1d9477f!8m2!3d41.2917917!4d19.5084236!16s%2Fg%2F11w_mxs5hn?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D"
           target="_blank"
+          className="hover:text-[#d12929]"
+        >
+          📍 Plepa Durres
+        </a>
+
+        <a
+          href="tel:0695467048"
           className="hover:text-[#d12929] flex items-center gap-2"
         >
           <Phone size={14} /> 069 546 7048
@@ -62,7 +74,7 @@ const Nav = () => {
 
       {/* Mobile Navigation (Hamburger menu) */}
       <div className="w-[50%] lg:hidden flex items-center justify-end">
-      {/* <Sheet>
+        {/* <Sheet>
           <SheetTrigger>
             <button className="w-[50px] h-[50px] border-[0.5px] flex items-center justify-center rounded-lg">
               <PanelsTopLeft className="text-gray-500" />
@@ -79,7 +91,7 @@ const Nav = () => {
             </SheetHeader>
           </SheetContent>
         </Sheet> */}
-        <MobileMenu/>
+        <MobileMenu />
       </div>
     </div>
   );
